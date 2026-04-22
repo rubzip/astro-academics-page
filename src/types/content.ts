8c361d995740ae2f9506bad6a0629ac8d42a0d28
Rubén <74114026+rubzip@users.noreply.github.com>
2026-04-22 14:46:43 +0200

    Create content.ts

---
+++ src/types/content.ts
@@ -0,0 +1,46 @@
+export interface Bio {
+
+}
+
+export interface CVItem {
+  name: string;
+  institution: string;
+  date: string;
+  description: string;
+}
+
+export interface CV {
+  name: string;
+  title: string;
+  experience: CVItem[];
+  education: CVItem[];
+}
+
+export interface BasePage {
+  title: string;
+  description: string;
+  tags: string[];
+  date: string;
+}
+
+export interface Blog extends BasePage {
+  author?: string;
+}
+
+export interface Project extends BasePage {
+  external_url?: string;
+}
+
+export interface Publication extends BasePage {
+  external_url?: string;
+}
+
+export interface Talk extends BasePage {
+  event?: string;
+  external_url?: string;
+}
+
+export interface Teaching extends BasePage {
+  institution?: string;
+  external_url?: string;
+}
